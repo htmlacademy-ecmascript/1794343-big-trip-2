455f2b908a1d9355099bd840cb3a2d3c72ce92d7
Anna-Poly <korobulya@mail.ru>
2024-11-07 13:10:02 +0300

    Adds working sorting

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -4,17 +4,23 @@ import EmptyListView from '../view/empty-list-view.js';
 import { render } from '../framework/render.js';
 import PointPresenter from './point-presenter.js';
 import { updateItem } from '../utils/common.js';
+import { sortDate, sortPrice, sortTime } from '../utils/event.js';
+import { SortingType } from '../const.js';
 
 export default class Presenter {
   #points = [];
   #destinations = [];
   #offers = [];
+
   #container = null;
   #eventModel = null;
-  #sortingComponent = new SortingView();
+
+  #sortingComponent = null;
   #eventListComponent = new EventListView();
   #noEventsComponent = new EmptyListView();
   #pointPresenters = new Map();
+  #currentSortType = SortingType.DAY;
+  #sourcedPoints = [];
 
   constructor({container, eventModel}) {
     this.#container = container;
@@ -23,6 +29,7 @@ export default class Presenter {
 
   init() {
     this.#points = this.#eventModel.points;
+    this.#sourcedPoints = [...this.#eventModel.points];
     this.#destinations = this.#eventModel.destinations;
     this.#offers = this.#eventModel.offers;
 
@@ -36,10 +43,40 @@ export default class Presenter {
 
   #handlePointChange = (updatedPoint) => {
     this.#points = updateItem(this.#points, updatedPoint);
+    this.#sourcedPoints = updateItem(this.#sourcedPoints, updatedPoint);
     this.#pointPresenters.get(updatedPoint.id).init(updatedPoint, this.#destinations, this.#offers);
   };
 
+  #sortPoints(sortingType) {
+    switch (sortingType) {
+      case SortingType.DAY:
+        this.#points.sort(sortDate);
+        break;
+      case SortingType.PRICE:
+        this.#points.sort(sortPrice);
+        break;
+      case SortingType.TIME:
+        this.#points.sort(sortTime);
+        break;
+      default:
+        this.#points = [...this.#sourcedPoints];
+    }
+    this.#currentSortType = sortingType;
+  }
+
+  #handleSortTypeChange = (sortingType) => {
+    if (this.#currentSortType === sortingType) {
+      return;
+    }
+    this.#sortPoints(sortingType);
+    this.#clearEventList();
+    this.#renderEventListItems(this.#points, this.#destinations, this.#offers);
+  };
+
   #renderSorting() {
+    this.#sortingComponent = new SortingView({
+      onSortTypeChange: this.#handleSortTypeChange
+    });
     render(this.#sortingComponent, this.#container);
   }
 
@@ -66,8 +103,6 @@ export default class Presenter {
   #clearEventList() {
     this.#pointPresenters.forEach((presenter) => presenter.destroy());
     this.#pointPresenters.clear();
-    //this.#renderedTaskCount = TASK_COUNT_PER_STEP;
-    //remove(this.#loadMoreButtonComponent);
   }
 
   #renderEmptyList (points) {
--- src/utils/event.js
+++ src/utils/event.js
@@ -32,4 +32,33 @@ function isEventBeInFuture(dueDate) {
   return dueDate && dayjs().isBefore(dueDate, 'D');
 }
 
-export {humanizeEventDueDate, getTimeDuration, isEventToday, isEventHappened, isEventBeInFuture};
+function getWeightForNull(eventA, eventB) {
+  if (eventA === null && eventB === null) {
+    return 0;
+  }
+  if (eventA === null) {
+    return 1;
+  }
+  if (eventB === null) {
+    return -1;
+  }
+  return null;
+}
+function sortDate(eventA, eventB) {
+  const weight = getWeightForNull(eventA.dateFrom, eventB.dateFrom);
+  return weight ?? dayjs(eventB.dateFrom).diff(dayjs(eventA.dateFrom));
+}
+
+function sortPrice(eventA, eventB) {
+  const weight = getWeightForNull(eventA.basePrice, eventB.basePrice);
+  return weight ?? (eventB.basePrice - eventA.basePrice);
+}
+
+function sortTime(eventA, eventB) {
+  const weight = getWeightForNull(eventA, eventB);
+  const durationA = dayjs(eventA.dateFrom).diff(dayjs(eventA.dateTo));
+  const durationB = dayjs(eventB.dateFrom).diff(dayjs(eventB.dateTo));
+  return weight ?? (durationA - durationB);
+}
+
+export {humanizeEventDueDate, getTimeDuration, isEventToday, isEventHappened, isEventBeInFuture, sortDate, sortPrice, sortTime};
